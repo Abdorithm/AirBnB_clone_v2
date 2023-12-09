249676147456ab096d46ace13719d6125446d8c5
Abdelrahman Magdi <abdomagdi300@gmail.com>
2023-12-09 14:32:41 -0800

    hot fix

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -4,20 +4,20 @@ from fabric.api import *
 import time
 import os
 
-
 env.hosts = ['35.175.104.84', '100.26.151.210']
 env.user = 'ubuntu'
 
 
 def do_pack():
     """Generates a .tgz archive from the contents of the web_static"""
-    local("mkdir -p versions")
-    archive_path = 'versions/web_static_{}.tgz'.format(
-        time.strftime('%Y%m%d%H%M%S'))
-    results = local('tar -cvzf {} web_static'.format(archive_path))
-    if results.successed:
+    try:
+        local("mkdir -p versions")
+        archive_path = 'versions/web_static_{}.tgz'.format(
+                time.strftime('%Y%m%d%H%M%S'))
+        results = local('tar -cvzf {} web_static'.format(archive_path))
         return archive_path
-    return None
+    except Exception:
+        return None
 
 
 def do_deploy(archive_path):
@@ -29,8 +29,8 @@ def do_deploy(archive_path):
     release_version = '/data/web_static/releases/{}'.format(archive_file[:-4])
     put(archive_path, '/tmp/')
     run('mkdir -p {}'.format(release_version))
-    run('tar -xzf /tmp/{} -C {}'.format(archive_file, release_version))
-    run('rm /tmp/{}'.format(archive_file))
+    run('sudo tar -xzf /tmp/{} -C {}'.format(archive_file, release_version))
+    run('sudo rm -f /tmp/{}'.format(archive_file))
     run('sudo mv {}/web_static/* {}'.format(release_version, release_version))
     run('sudo rm -rf /data/web_static/releases/web_static')
     run('sudo rm -rf /data/web_static/current')
@@ -41,8 +41,10 @@ def do_deploy(archive_path):
 
 
 def deploy():
-    """Creates and distributes an archive to your web servers"""
-    archive_path = do_pack()
-    if archive_path is None:
+    """Create and distributes an archive to web servers"""
+    try:
+        path = do_pack()
+        return do_deploy(path)
+    except Exception:
+        print("Error occurred")
         return False
-    return do_deploy(archive_path)
