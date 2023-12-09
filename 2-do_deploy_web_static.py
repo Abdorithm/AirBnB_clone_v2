a0b8a477ea4847140d7f5031cf8d66a3003846d1
Abdelrahman Magdi <abdomagdi300@gmail.com>
2023-12-09 14:44:31 -0800

    hotter fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -29,12 +29,12 @@ def do_deploy(archive_path):
     release_version = '/data/web_static/releases/{}'.format(archive_file[:-4])
     put(archive_path, '/tmp/')
     run('mkdir -p {}'.format(release_version))
-    run('sudo tar -xzf /tmp/{} -C {}'.format(archive_file, release_version))
-    run('sudo rm -f /tmp/{}'.format(archive_file))
-    run('sudo mv {}/web_static/* {}'.format(release_version, release_version))
-    run('sudo rm -rf /data/web_static/releases/web_static')
-    run('sudo rm -rf /data/web_static/current')
-    run('sudo ln -s {} /data/web_static/current'.format(release_version))
+    run('tar -xzf /tmp/{} -C {}'.format(archive_file, release_version))
+    run('rm -f /tmp/{}'.format(archive_file))
+    run('mv {}/web_static/* {}'.format(release_version, release_version))
+    run('rm -rf /data/web_static/releases/web_static')
+    run('rm -rf /data/web_static/current')
+    run('ln -s {} /data/web_static/current'.format(release_version))
 
     print('New version deployed!')
     return True
